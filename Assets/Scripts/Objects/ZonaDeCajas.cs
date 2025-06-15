910ec9a6d1837846512493460022052678a59576
Alejudas <variasveses@gmail.com>
2025-06-15 16:39:48 -0500

    Codigos pae

---
+++ Assets/Scripts/Objects/ZonaDeCajas.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonaDeCajas : MonoBehaviour
+
+{
+    [SerializeField] private int cantidadNecesaria = 3;
+
+    private HashSet<GameObject> objetosEnZona = new HashSet<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (EsObjetoValido(other.gameObject))
+        {
+            objetosEnZona.Add(other.gameObject);
+
+            Debug.Log("Entró: " + other.name);
+
+            if (objetosEnZona.Count >= cantidadNecesaria)
+            {
+                Debug.Log("¡Cantidad requerida alcanzada!");
+                // Ejecuta la acción deseada aquí
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (objetosEnZona.Contains(other.gameObject))
+        {
+            objetosEnZona.Remove(other.gameObject);
+            Debug.Log("Salió: " + other.name);
+        }
+    }
+
+    private bool EsObjetoValido(GameObject obj)
+    {
+        // Aquí validas si es uno de los que deben contarse
+        // Por ejemplo, usando tag, componente o nombre
+        return obj.CompareTag("ObjetoValido");
+    }
+}
+
+
